3a7479fe6a7bdc5a34a568346aa1a683f2040448
SalazarV4 <diegosalazar.v4@gmail.com>
2025-10-21 16:28:21 -0500

    added EIA api extractor

--- src/oil_optimization/utils/io_helpers.py
+++ src/oil_optimization/utils/io_helpers.py
@@ -1,7 +1,12 @@
 from pathlib import Path
 import yaml
+import pandas as pd
 
-def read_yaml(path_to_yaml: Path):
+def read_yaml(path_to_yaml: str):
+    """Read yaml file"""
     with open(path_to_yaml, encoding='utf-8') as yaml_file:
         content = yaml.safe_load(yaml_file)
         return content
+
+def save_csv(df: pd.DataFrame, path: str):
+    df.to_csv(path, index=False)
