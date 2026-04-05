b5de24932ab12d8cafa4e9d561ae4d9f6695c778
SalazarV4 <140282366+SalazarV4@users.noreply.github.com>
2026-04-05 03:41:05 +0000

    Adding logging, and other enhancements to EIA extractor

--- src/oil_optimization/utils/io_helpers.py
+++ src/oil_optimization/utils/io_helpers.py
@@ -7,6 +7,3 @@ def read_yaml(path_to_yaml: str):
     with open(path_to_yaml, encoding='utf-8') as yaml_file:
         content = yaml.safe_load(yaml_file)
         return content
-
-def save_csv(df: pd.DataFrame, path: str):
-    df.to_csv(path, index=False)
